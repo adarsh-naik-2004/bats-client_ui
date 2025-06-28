60dbc3b5f03817f20048dd6ae561647fc7f4c4f4
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-06-28 16:35:31 +0530

    stepper fix 5

--- src/components/custom/store-select.tsx
+++ src/components/custom/store-select.tsx
@@ -1,23 +1,33 @@
-'use client';
-import React from 'react';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
-import { Store } from '@/lib/types';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { MapPin } from 'lucide-react';
+"use client";
 
-const StoreSelect = ({ shops }: { shops: { data: Store[] } }) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Store } from "@/lib/types";
+import { MapPin } from "lucide-react";
 
+export default function StoreSelect({
+  shops,
+  onSelect,
+  initialValue,
+}: {
+  shops: { data: Store[] };
+  onSelect: (shopId: string) => void;
+  initialValue?: string;
+}) {
   const handleValueChange = (value: string) => {
-    router.push(`/?shopId=${value}`);
+    onSelect(value);
   };
 
   return (
     <div className="relative">
       <Select
         onValueChange={handleValueChange}
-        defaultValue={searchParams.get('shopId') || ''}
+        value={initialValue}
       >
         <SelectTrigger className="w-[280px] border-gray-700 focus:ring-2 focus:ring-orange-500 focus:border-transparent rounded-full bg-gray-800/50 backdrop-blur-sm hover:bg-gray-800/70 transition-all duration-300 py-3 px-4 shadow-sm">
           <div className="flex items-center gap-3">
@@ -42,6 +52,4 @@ const StoreSelect = ({ shops }: { shops: { data: Store[] } }) => {
       </Select>
     </div>
   );
-};
-
-export default StoreSelect;
+}
